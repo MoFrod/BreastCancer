2ec189d6762cbf81e79cb217e9661f89c1e9f8ff
MoFrod <morgan@frodsham.net>
2022-11-12 18:58:49 +0000

    Munge code updated as per data cleaning instructions

--- munge/01-A.R
+++ munge/01-A.R
@@ -4,4 +4,35 @@ library(mlbench)
 # Load the data specified in project description
 data("BreastCancer")
 
-# Create data set for initial investigation 
+# Create tibble for initial investigation 
+BC1 <- as_tibble(BreastCancer)
+
+# Confirm variable type in BC1
+ls(BC1) # List the categories
+
+class(BC1$Id) # Character
+class(BC1$Cl.thickness) # Ordered Factor
+class(BC1$Cell.size) # Ordered Factor
+class(BC1$Cell.shape) # Ordered Factor
+class(BC1$Marg.adhesion) # Ordered Factor
+class(BC1$Epith.c.size) # Ordered Factor
+class(BC1$Bare.nuclei) # Factor
+class(BC1$Bl.cromatin) # Factor
+class(BC1$Normal.nucleoli) # Factor
+class(BC1$Mitoses) # Factor
+class(BC1$Class) # Factor
+
+# Change factors into quantitative variables
+BC1$Cl.thickness <- as.numeric(BC1$Cl.thickness)
+BC1$Cell.size <- as.numeric(BC1$Cell.size)
+BC1$Cell.shape <- as.numeric(BC1$Cell.shape)
+BC1$Marg.adhesion <- as.numeric(BC1$Marg.adhesion)
+BC1$Epith.c.size <- as.numeric(BC1$Epith.c.size)
+BC1$Bare.nuclei <- as.numeric(BC1$Bare.nuclei)
+BC1$Bl.cromatin <- as.numeric(BC1$Bl.cromatin)
+BC1$Normal.nucleoli <- as.numeric(BC1$Normal.nucleoli)
+BC1$Mitoses <- as.numeric(BC1$Mitoses)
+
+# Remove all rows where there are missing values
+BC1 <- na.omit(BC1)
+
