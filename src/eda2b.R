1e03c3e453a072870a32e8106d2093cc9713b4d7
MoFrod <morgan@frodsham.net>
2022-11-24 11:47:54 +0000

    Fixed LASSO and updated report

--- src/eda2b.R
+++ src/eda2b.R
@@ -71,42 +71,47 @@ lasso_yhat_red = ifelse(lasso_phat_red > 0.5, 1, 0)
 
 #LASSO w. SPLIT DATA
 
+# Extract the response variable
+tClass <- train[,7]
+
+#Extract the predictor variables
+xt <- train[,1:6]
+
 ##Cross-validate LASSO for training data
-train_l = as.matrix(train) #Covert to matrix for cv
-lasso_cv_fit_train = cv.glmnet(train_l, train$Class, family = "binomial", alpha = 1, standardize = FALSE, lambda = grid, type.measure = "class")
+train_l = as.matrix(xt) #Covert to matrix for cv
+lasso_cv_fit_train = cv.glmnet(train_l, tClass, family = "binomial", alpha = 1, standardize = FALSE, lambda = grid, type.measure = "class")
 plot(lasso_cv_fit_train) # Plot cross-validation
 
 ##Identify the optimal value for the turning parameter
-(lambda_lasso_min_train = lasso_cv_fit_train$lambda.min) #0.01963041
-(which_lambda_lasso_train = which(lasso_cv_fit_train$lambda == lambda_lasso_min_train)) #84
+(lambda_lasso_min_train = lasso_cv_fit_train$lambda.min) #0.002104904
+(which_lambda_lasso_train = which(lasso_cv_fit_train$lambda == lambda_lasso_min_train)) #96
 
 # Extract corresponding mean MSE
-MSE <- lasso_cv_fit_train$cvm[which_lambda_lasso_train] #0
+lasso_cv_fit_train$cvm[which_lambda_lasso_train] # 0.02380952
 
 ##Fit a model with LASSO penalty for each value of the turning parameter to training data
-lasso_fit_train = glmnet(train, train$Class, family = "binomial", alpha = 1, standardize = FALSE, lambda = lambda_lasso_min_train)
+lasso_fit_train = glmnet(xt, tClass, family = "binomial", alpha = 1, standardize = FALSE, lambda = lambda_lasso_min_train)
+
+## Find the parameter estimates associated with optimal value of the tuning parameter
+coef(lasso_fit_train, s=lambda_lasso_min_train)
 
 ##Compute fitted values for the validation data
-test_l <- as.matrix(test) #Test data as matrix
-lasso_phat_test.n = as.numeric(predict(lasso_fit_train, test_l, s=lambda_lasso_min_train, type="response"))
-lasso_yhat_test.n = ifelse(lasso_phat_test.n > 0.5, 1, 0)
+xtest <- test[,1:6] ##Extract the predictor variables
+test_l <- as.matrix(xtest) #Test data as matrix
+lasso_phat_test = predict(lasso_fit_train, test_l, s=lambda_lasso_min_train, type="response")
+lasso_yhat_test = ifelse(lasso_phat_test > 0.5, 1, 0)
 
 ##Compute test error
-1-mean(test$Class == lasso_yhat_test.n) #0%
-1-mean(test_l == lasso_yhat_test.n) #0.8571429. If i change to test_l[,7] it goes back to zero.
+1-mean(test$Class == lasso_yhat_test) #0.05839416, 5.84%
 
 ## Evaluate the model
-lasso_phat_test = predict(lasso_fit_train, test_l, s=lambda_lasso_min_train, type="response") #Not as.numeric
+lasso_phat_test = predict(lasso_fit_train, test_l, s=lambda_lasso_min_train, type="response") 
 pred.lasso <- prediction(lasso_phat_test, test$Class)
 roc.perf.lasso = performance(pred.lasso, measure = "tpr", x.measure = "fpr")
 auc.train.lasso <- performance(pred.lasso, measure = "auc")
 auc.train.lasso <- auc.train.lasso@y.values
 
-# Plot LASSO on ROC
+## Plot LASSO on ROC
 plot(roc.perf.lasso, col="#1f78b4", main="Plot : LASSO Regression ROC", cex.main=0.8)
 abline(a=0, b= 1)
 text(x = .25, y = .65, paste("AUC = ", round(auc.train.lasso[[1]],3), sep = ""))
-
-# ROC - NOT WORKING
-lasso_test_roc <- roc(test_l[,7] ~ lasso_phat_test.n, plot = TRUE, print.auc = TRUE, main="Plot : LASSO Regression ROC", cex.main=0.8, col="#1f78b4")
-lasso_test_roc <- roc(train$Class ~ lasso_phat_test.n, plot = TRUE, print.auc = TRUE, main="Plot : LASSO Regression ROC", cex.main=0.8, col="#1f78b4")
